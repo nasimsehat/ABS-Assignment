f43c898f2e0193405a4594625f6c9ca73d62b074
Jake <tan.zijie9@gmail.com>
2019-11-08 00:05:24 +0100

    Add project files.

---
+++ ABS_Assignment/Templates/ABS Assignment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+// <Custom "using" statements>
+
+
+
+// </Custom "using" statements>
+
+
+#region padding (this ensures the line number of this file match with those in the code editor of the C# Script component
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
+
+public partial class ABS_Assignment : GH_ScriptInstance
+{
+    #region Do_not_modify_this_region
+    private void Print(string text) { }
+    private void Print(string format, params object[] args) { }
+    private void Reflect(object obj) { }
+    private void Reflect(object obj, string methodName) { }
+    public override void InvokeRunScript(IGH_Component owner, object rhinoDocument, int iteration, List<object> inputs, IGH_DataAccess DA) { }
+    public RhinoDoc RhinoDocument;
+    public GH_Document GrasshopperDocument;
+    public IGH_Component Component;
+    public int Iteration;
+    #endregion
+
+
+    private void RunScript(object x, object y, ref object A)
+    {
+        // <Custom code>
+
+        
+
+        // </Custom code>
+    }
+
+    // <Custom additional code>
+
+
+
+    // </Custom additional code>
+}
---
+++ ABS_Assignment/Templates/ABS Assignment_Extra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+// <Custom "using" statements>
+
+
+
+// </Custom "using" statements>
+
+
+public partial class ABS_Assignment
+{
+    // <Custom additional code>
+
+
+
+    // </Custom additional code>
+}
