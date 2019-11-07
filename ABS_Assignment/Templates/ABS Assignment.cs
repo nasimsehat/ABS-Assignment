94e7146d45a356a3c83892e594f0ff373cc97196
Jake <tan.zijie9@gmail.com>
2019-11-08 00:22:59 +0100

    Jake branch edited from PC

--- ABS_Assignment/Templates/ABS Assignment.cs
+++ ABS_Assignment/Templates/ABS Assignment.cs
@@ -55,7 +55,7 @@ public partial class ABS_Assignment : GH_ScriptInstance
     {
         // <Custom code>
 
-        //new branch from PC from jake branch_Edited
+        //Jake edited Jake_Branch from PC
 
         // </Custom code>
     }
